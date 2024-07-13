a4a617ef9966ea1a11352d52ecb153024ea8c4ac
lieanaaa <lianasalsabila50@smk.belajar.id>
2024-07-13 11:46:29 +0800

    Membuat Struktur Input

---
+++ perhitungan_cekmata.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+struct CekMata {
+    std::string namaPasien;
+    int usia;
+    double tinggiBadan;
+    double jarakPandang;
+    int hasilTesMata;
+    std::string statusKesehatanMata;
+};
